90f96acfc7ebd504e1d1671b69676e5aaa7607d0
Kevin Laux <42847161+KevinLaux@users.noreply.github.com>
2023-05-17 19:23:28 -0400

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,5 @@
-Write-Output $BranchName
+Write-Output "Attempting to add file to $BranchName"
+git checkout -b $branchName
+git add $filePath
+git commit -m "Add events for $date"
+git push origin $branchName
